4c17b724a7585bf05c251aa246bd0d2e5699d64d
Timoty Gielkens <timo@Timotys-MacBook-Air.local>
2022-11-06 17:26:47 +0100

    m

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from "./App"
+import { useState, useEffect } from 'react'
+
+const Counter = () => {
+  
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => { 
+    if (counter >= 10) {
+      ReactDOM.render(<p>Counter has reached 10!</p>, document.getElementById('left'));
+    } 
+  }, [counter]);
+
+  return (
+    <div>
+      <button onClick={ () => { setCounter(counter - 1) } }>-</button>
+      <p>{ counter }</p>
+      <button onClick={ () => { setCounter(counter + 1 )} }>+</button>
+    </div>
+  );
+}
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-
+ReactDOM.render(<Counter/>, document.getElementById('left'));
 
 
