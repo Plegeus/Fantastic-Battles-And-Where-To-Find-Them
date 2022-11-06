8a036452ad0ead9c47de4d983afed3f2e5e4438b
Timoty Gielkens <timo@Timotys-MacBook-Air.local>
2022-11-06 14:58:52 +0100

    m

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,27 @@
 import React from "react";
 
 const App = () => {
-  return <h1>React!</h1>;
+  return( 
+    <div align="center" class="vertical">
+      <div id="top">
+        TOP
+      </div>
+      <div class="horizontal">
+        <div id="left">
+          LEFT
+        </div>
+        <div id="center">
+          CENTER
+        </div>
+        <div id="right">
+          RIGHT
+        </div>
+      </div>
+      <div id="bottom">
+        BOTTOM
+      </div>
+    </div>
+  );
 }
 
 export default App;
