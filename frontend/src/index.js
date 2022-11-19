de39166f8c7439a7c8cad5e35ff60fc8a05a1012
Timoty Gielkens <timo@Timotys-MacBook-Air.local>
2022-11-20 00:19:21 +0100

    m

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from "./app";
 
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+//ReactDOM.render(<p>Hello World</p>, document.getElementById('root'));
 
 
 
