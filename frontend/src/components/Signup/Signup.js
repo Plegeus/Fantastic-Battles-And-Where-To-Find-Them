bac63be46f134679b4d5cb66ec957ef4ba578269
RobbeThielemans <Robbe.Thielemans@vub.be>
2022-12-15 12:16:33 +0100

    Front-End Form validation Signup Page

--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -2,14 +2,64 @@ import "./Signupstyles.css";
 import SignupPage1 from "./SignupPage1";
 import SignupPage2 from "./SignupPage2";
 import SignupPage1Bottom from "./SignupPage1Bottom";
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
+import SignupContext from "./Signup.context";
 
 import "./SignupStepsstyles.css"
 
 
 const Signup = () => {
 
+    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     const [page, setPage] = useState(1)
+    const {
+        Email,
+        setEmail,
+        Username,
+        setUsername,
+        Password,
+        setPassword
+    } = useContext(SignupContext);
+
+    const [FormErrors, setFormErrors] = useState({});
+
+
+    const handleRegister = (e) => {
+        e.preventDefault()
+        setFormErrors(handleErrors)
+    }
+
+    useEffect(() => {
+        if (Object.keys(FormErrors).length === 0) {
+            console.log(Email, Password, Username)
+        }
+        else if (!Email || !regex.test(Email)) {
+            setPage(1)
+        }
+        else if (!Username || !Password) {
+            setPage(2)
+        }
+
+    }, [FormErrors])
+
+    const handleErrors = () => {
+        const errors = {};
+        if (!Email || !regex.test(Email)) {
+            if (!Email) {
+                errors.email = "Email is required"
+            }
+            else {
+                errors.email = "Email is not valid"
+            }
+        }
+        if (!Username) {
+            errors.username = "Username is required"
+        }
+        if (!Password) {
+            errors.password = "Password is required"
+        }
+        return errors
+    }
 
     function decideIcons(page) {
         setPage(page)
@@ -45,9 +95,10 @@ const Signup = () => {
                 <div className="loginText">
                     <h1>Signup</h1>
                 </div>
-                <form className="loginCover">
+                <form onSubmit={handleRegister} className="loginCover">
                     {
-                        page === 1 ? <SignupPage1 /> : page === 2 ? <SignupPage2 /> : null
+                        page === 1 ? <SignupPage1 emailError={FormErrors.email} /> :
+                            page === 2 ? <SignupPage2 usernameError={FormErrors.username} passwordError={FormErrors.password} /> : null
                     }
                     {
                         page < 2 && (
@@ -68,7 +119,7 @@ const Signup = () => {
                         page === 1 ? <SignupPage1Bottom /> : null
                     }
 
-                    
+
 
                     <div className="SignupSteps">
                         <span id="circle1" className="circle"></span>
