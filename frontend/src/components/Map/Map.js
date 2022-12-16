fc5495cbf110830077176ec0bd95b137c9b6c442
Rob Leemans <93993725+FalconKnight54@users.noreply.github.com>
2022-12-16 11:41:45 +0100

    stuksken overview zodat ozzen bobbers wa voor zijn accountje kan werken :))

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import markerIconPng from "leaflet/dist/images/marker-icon.png"
 import {Icon} from 'leaflet'
@@ -24,20 +24,20 @@ function Map () {
           <div id="add_battle_pane">
             <form id="addBattle">
               <div id="coordinate"></div>
-              <label for="name">Name:</label><br></br>
-              <input type="text" id="name" name="name" value="" required /><br></br>
-              <label for="victor">Victor:</label><br></br>
-              <input type="text" id="victor" name="victor" value="" required /><br></br>
-              <label for="vanquished">Vanquished:</label><br></br>
-              <input type="text" id="vanquished" name="vanquished" value="" required /><br></br>
-              <label for="victorious_commander">Victorious Commander:</label><br></br>
-              <input type="text" id="victorious_commander" name="victorious_commander" value="" required /><br></br>
-              <label for="vanquished_commander">Vanquished Commander:</label><br></br>
-              <input type="text" id="vanquished_commander" name="vanquished_commander" value="" required /><br></br>
-              <label for="victorious_deaths">Victorious Deaths:</label><br></br>
-              <input type="any" id="victorious_deaths" name="victorious_deaths" value="" required /><br></br>
-              <label for="vanquished_deaths">Vanquished Deaths:</label><br></br>
-              <input type="any" id="vanquished_deaths" name="vanquished_deaths" value="" required /><br></br>
+              <label htmlFor="name">Name:</label><br></br>
+              <input type="text" id="name" name="name"  required /><br></br>
+              <label htmlFor="victor">Victor:</label><br></br>
+              <input type="text" id="victor" name="victor" required /><br></br>
+              <label htmlFor="vanquished">Vanquished:</label><br></br>
+              <input type="text" id="vanquished" name="vanquished" required /><br></br>
+              <label htmlFor="victorious_commander">Victorious Commander:</label><br></br>
+              <input type="text" id="victorious_commander" name="victorious_commander" required /><br></br>
+              <label htmlFor="vanquished_commander">Vanquished Commander:</label><br></br>
+              <input type="text" id="vanquished_commander" name="vanquished_commander" required /><br></br>
+              <label htmlFor="victorious_deaths">Victorious Deaths:</label><br></br>
+              <input type="any" id="victorious_deaths" name="victorious_deaths" required /><br></br>
+              <label htmlFor="vanquished_deaths">Vanquished Deaths:</label><br></br>
+              <input type="any" id="vanquished_deaths" name="vanquished_deaths" required /><br></br>
               <br></br>
             </form> 
           </div>
@@ -46,7 +46,11 @@ function Map () {
         <TileLayer
           url='https://api.maptiler.com/maps/topo-v2/{z}/{x}/{y}.png?key=v21B0xhC8tSTZGn1gUwV'
           style={{ height: '100%', width: '100%' }}
-          
+          eventHandlers={{
+            mousemove: () => {
+              console.log('marker clicked')
+            },
+          }}
         />
         <Marker position={[51.505, -0.09]}icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})} eventHandlers={{
         click: (e) => {
--- frontend/src/components/Overview/Overview.js
+++ frontend/src/components/Overview/Overview.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import "./overview-tab.css"
+
+
 
 const Overview = () => {
   return (
     <div>
-        
+      <div id="button-div">
+		<button class="list-btn" onClick='listInator()'><i class="bi-list"> list</i></button>
+		<button class="tile-btn" onClick='tileInator()'><i class="bi-grid-fill">tiles</i></button>
+    </div>
+    <div class="filter"> </div> 
+    <div id="list-view">
+      <table id="battle-table"> 
+          <thead>
+              <tr> 
+                <th>Name</th> 
+                <th>Victor</th> 
+                <th>Vanquished</th> 
+                <th>Victorious Commander</th> 
+                <th>Vanquished Commander</th> 
+                <th>Victorious deaths</th> 
+                <th>Vanquished deaths</th>  
+              </tr> 
+          </thead>
+      </table> 
+    </div>
+    <div id="tile-view" class="grid-container"></div>
     </div>
   )
 }
