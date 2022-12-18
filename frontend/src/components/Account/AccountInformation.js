4eb5220a2fcb9a7100395627e3696463ddef4662
Timoty Gielkens <timo@Timotys-MacBook-Air.local>
2022-12-18 13:01:15 +0100

    Random picture based on username

--- frontend/src/components/Account/AccountInformation.js
+++ frontend/src/components/Account/AccountInformation.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import useFetch from "../../Util/useFetch";
 import "./AccountInformationStyles.css"
+import Picture from "./Picture";
 
 //fetch user information
 
@@ -30,8 +31,7 @@ const AccountInformation = (props) => {
 
         <div className="AccountInformation" >
             <div className="ProfilePicture">
-                <img src="https://www.w3schools.com/images/w3schools_green.jpg"
-                    width="300" height="300"></img>
+                <Picture username={"Plegeus"}/>
             </div>
 
             <div className="generalAccountInfo">
