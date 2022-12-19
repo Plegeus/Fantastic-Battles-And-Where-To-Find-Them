ba333ac002ab1bde3f0dcb27c57e235206592d44
RobbeThielemans <Robbe.Thielemans@vub.be>
2022-12-19 21:18:03 +0100

    reformart battlepage

--- frontend/src/components/BattlePage/BattlePage.js
+++ frontend/src/components/BattlePage/BattlePage.js
@@ -52,7 +52,7 @@ const BattlePage = () => {
             })
                 .catch(err => {
                     console.log("error");
-                    
+
                 })
         }
     }
@@ -83,7 +83,7 @@ const BattlePage = () => {
                             <button id="saveButton">
                                 Save
                             </button>
-                            {!CurrentConditions &&<button className="WeatherButton" onClick={getWeatherData}>Weather</button>}
+                            {!CurrentConditions && <button className="WeatherButton" onClick={getWeatherData}>Weather</button>}
                         </div>
                     </div>
 
@@ -94,9 +94,9 @@ const BattlePage = () => {
                         {CurrentConditions &&
                             <div id="theWeather">
                                 <img id="battlePic" src={"/WeatherIcons/" + String(CurrentConditions.currentConditions.icon) + ".png"} alt="A weather icon" width="50rem"></img>
-                                <p>Current Temperature: { Math.round((CurrentConditions.currentConditions.temp - 32) * 5/9)}° Celsius </p>
-                                <p>Current Weather Condition: { CurrentConditions.currentConditions.conditions } </p>
-                                <p>Current Weather Description { CurrentConditions.description } </p>
+                                <p>Current Temperature: {Math.round((CurrentConditions.currentConditions.temp - 32) * 5 / 9)}° Celsius </p>
+                                <p>Current Weather Condition: {CurrentConditions.currentConditions.conditions} </p>
+                                <p>Current Weather Description {CurrentConditions.description} </p>
                             </div>
                         }
                     </div>
