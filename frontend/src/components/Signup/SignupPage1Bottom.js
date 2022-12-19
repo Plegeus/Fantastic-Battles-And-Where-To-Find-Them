cb9c6f6a6b7a5306a5aade5f8ca4ff770387b580
RobbeThielemans <Robbe.Thielemans@vub.be>
2022-12-19 22:04:50 +0100

    Removed Facebook and login icons

--- frontend/src/components/Signup/SignupPage1Bottom.js
+++ frontend/src/components/Signup/SignupPage1Bottom.js
@@ -1,32 +1,10 @@
 import "./SignupPagesStyles.css";
-import { BsFacebook, BsGoogle } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 const SignupPage1Bottom = () => {
-
-    const facebookIcon = <BsFacebook className="EyeIcon"
-        size="2.3rem" color="blue"
-    />
-
-    const googleIcon = <BsGoogle className="EyeIcon"
-        size="2.3rem" color="red"
-    />
-
     return (
-        <div>
-            <div className="socialLogin">
-                <a href="#">
-                    <span className="facebookIcon">{facebookIcon}</span>
-                </a>
-                <a href="#">
-                    <span className="googleIcon">{googleIcon}</span>
-                </a>
-            </div>
-
-
-            <div className="login">
-                <p>Already an account? <Link to="/login" className="forgotPassword">LOGIN</Link></p>
-            </div>
+        <div className="login">
+            <p>Already an account? <Link to="/login" className="forgotPassword">LOGIN</Link></p>
         </div>
     )
 }
