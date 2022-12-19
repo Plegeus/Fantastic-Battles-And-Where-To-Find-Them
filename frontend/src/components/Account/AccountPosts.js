cf39a98305e6d441fefce89d6f00c82aa7c02513
Rob Leemans <93993725+FalconKnight54@users.noreply.github.com>
2022-12-19 17:33:35 +0100

    bobbers aksed me to add this

--- frontend/src/components/Account/AccountPosts.js
+++ frontend/src/components/Account/AccountPosts.js
@@ -14,8 +14,58 @@ const AccountPosts = (props) => {
     });
 
     return (
-        <div>
-            account
+        <div id="accountPosts">
+            <div id="listView">
+        <table id="battle-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Victor</th>
+              <th>Vanquished</th>
+              <th>Victorious Commander</th>
+              <th>Vanquished Commander</th>
+              <th>Victorious deaths</th>
+              <th>Vanquished deaths</th>
+            </tr>
+          </thead>
+          <tr>
+              <td>Name</td>
+              <td>Victor</td>
+              <td>Vanquished</td>
+              <td>Victorious Commander</td>
+              <td>Vanquished Commander</td>
+              <td>Victorious deaths</td>
+              <td>Vanquished deaths</td>
+          </tr>
+          <tr>
+              <td>Name</td>
+              <td>Victor</td>
+              <td>Vanquished</td>
+              <td>Victorious Commander</td>
+              <td>Vanquished Commander</td>
+              <td>Victorious deaths</td>
+              <td>Vanquished deaths</td>
+          </tr>
+          <tr>
+              <td>Name</td>
+              <td>Victor</td>
+              <td>Vanquished</td>
+              <td>Victorious Commander</td>
+              <td>Vanquished Commander</td>
+              <td>Victorious deaths</td>
+              <td>Vanquished deaths</td>
+          </tr>
+          <tr>
+              <td>Name</td>
+              <td>Victor</td>
+              <td>Vanquished</td>
+              <td>Victorious Commander</td>
+              <td>Vanquished Commander</td>
+              <td>Victorious deaths</td>
+              <td>Vanquished deaths</td>
+          </tr>
+        </table>
+      </div>
         </div>
     )
 }
